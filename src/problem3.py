b5e781b76e5f9f31f3df6c4910b3ad5d2649a825
guilfojm <guilfojm@rose-hulman.edu>
2019-03-19 21:58:24 -0400

    DONE

--- src/problem3.py
+++ src/problem3.py
@@ -3,8 +3,8 @@ Exam 1, problem 3.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Justin Guilfoyle.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import testing_helper
 import time
@@ -12,12 +12,12 @@ import time
 
 def main():
     """ Calls the   TEST   functions in this module. """
-    run_test_problem3a()
+    #run_test_problem3a()
     run_test_problem3b()
 
 
 ###############################################################################
-# TODO: 2.  READ the doc-string for the  sum_of_digits  and   is_prime
+# DONE: 2.  READ the doc-string for the  sum_of_digits  and   is_prime
 # functions defined below.  They are the same as what you have seen before.
 # After you UNDERSTAND the doc-string (JUST the doc-string, NOT the code),
 # ASKING QUESTIONS AS NEEDED, change the above _TODO_ to DONE.
@@ -190,7 +190,7 @@ def problem3a(a, b):
       -- problem3a(8, 2) = 11 + 23 + 29 + 41 + 43 + 47 + 61  (which is 255)
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ###########################################################################
@@ -198,8 +198,17 @@ def problem3a(a, b):
     #    **  For full credit you must appropriately
     #    **  use (call) the appropriate function(s) that are DEFINED ABOVE.
     ###########################################################################
-
-
+    total = 0
+    for k in range(a, a**b):
+        if is_prime(k):
+            x = sum_of_digits(k)
+            if is_prime(x):
+                total = total + k
+            if is_prime(x) == False:
+                total = total
+        if is_prime(k) == False:
+            total = total
+    return total
 def run_test_problem3b():
     """ Tests the   problem3b   function. """
     print()
@@ -262,7 +271,7 @@ def problem3b(m, x):
     What goes out:
       -- Returns True if the number of doubly prime integers from m**2 to m**4,
          not counting m**4, is greater than or equal to x.
-          eturns False otherwise.
+          returns False otherwise.
     Side effects:   None.
     Examples:
       -- problem3b(6, 4) returns
@@ -277,7 +286,7 @@ def problem3b(m, x):
            which is approximately 12.020144157845959.
      """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #          Tests have been written for you (above).
     ###########################################################################
     ###########################################################################
@@ -285,7 +294,20 @@ def problem3b(m, x):
     #    **  For full credit you must appropriately
     #    **  use (call) the appropriate function(s) that are DEFINED ABOVE.
     ###########################################################################
-
+    total = 0
+    for k in range(m**2, m**4):
+        if is_prime(k):
+            x = sum_of_digits(k)
+            if is_prime(x):
+                total = total + k
+            if is_prime(x) == False:
+                total = total
+        if is_prime(k) == False:
+                total = total
+    if total < x:
+        return False
+    if total > x:
+        return True
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
